123b8a2398c96354fde92adcf5b29b2010ac4cac
Jesse <jesse.islam@gmail.com>
2020-03-12 13:06:24 -0400

    Current state posted as issue to sbcb

--- functionInProgress.R
+++ functionInProgress.R
@@ -10,11 +10,13 @@ library(data.table)
 
 brierScoreKMCens <- function(predSurvs, times, newData) {
   # inverse probability censoring weights
-  # note that we remove the last time, as this would give us 0. I shift all times by 1.
-  # IPCW=summary(survfit(Surv(time, event==0)~1, newData),c(min(times),head(times,-1)))$surv
+  # Note that we take probabilities right before the drops.
+  
+  #DIFFERENCES START HERE
   fitCens <- prodlim::prodlim(Hist(time, event != 0) ~ 1, newData, reverse = TRUE)
   IPCW.subject.times <- prodlim::predictSurvIndividual(fitCens, lag = 1) # G(t-|X)
-
+  #DIFFERENCES END HERE
+  
   # Empty matrix that will be filled in with the following loop
   Score <- matrix(NA, nrow(predSurvs), ncol(predSurvs))
   IPCWMatrix <- matrix(NA, nrow(predSurvs), ncol(predSurvs))
@@ -44,83 +46,17 @@ brierScoreKMCens <- function(predSurvs, times, newData) {
   return(Err)
 }
 
-set.seed(18)
-
-astrain <- simActiveSurveillance(278)
-astrain$event <- astrain$event != 0
-data.table::setorder(astrain, time, -event)
-astrainX <- model.matrix(event ~ . - time, data = astrain)[, -c(1)]
-astrainY <- data.matrix(astrain[, c(9, 8)])
-
-newData <- simActiveSurveillance(208)
-newData$event <- newData$event != 0
-data.table::setorder(newData, time, -event)
-newDataX <- model.matrix(event ~ . - time, data = newData)[, -c(1)]
-newDataY <- data.matrix(newData[, c(9, 8)])
-
-times <- sort(unique(newData$time))
-
-cbModel <- fitSmoothHazard.fit(astrainX, astrainY, family = "glmnet", time = "time", event = "event", alpha = 0,formula_time = ~log(time), ratio = 100, standardize = TRUE)
-absRiskcb <- absoluteRisk(object = cbModel, newdata = newDataX, time = times, s = "lambda.1se")
-absRiskcb[, -c(1)] <- 1 - absRiskcb[, -c(1)] # make it survival probabilities
-absRiskcb <- absRiskcb[-c(1), -c(1)] # remove extra time at 0. Also remove the first column (times) as it matches perfectly now
-
-Err <- brierScoreKMCens(predSurvs = absRiskcb, times = times, newData = newData)
-
-
-
-# fit a cox model
-coxfit <- coxph(Surv(time, event != 0) ~ ., data = astrain, x = TRUE)
-# Get Cox survivals
-coxPred <- summary(survfit(coxfit, newdata = newData), times = times)$surv
-
-plot(times, rowMeans(coxPred), type = "l", col = "red", ylab = "survival")
-lines(times, rowMeans(absRiskcb))
-legend(x = "topright", legend = c("cox", "cbGompertz"), col = c("red", "black"))
-title("RED IS COX")
-
-
-# calculate the IPA/brier scores
-X2 <- Score(list("PredictionModel" = coxfit), data = newData, formula = Surv(time, event != 0) ~ 1, summary = "ipa", se.fit = 0L, metrics = "brier", contrasts = FALSE, times = times)
-
-
-
-
-# restructuring to make plotting easier
-results <- data.frame(riskRegression = X2$Brier$score$Brier[X2$Brier$score$model == "PredictionModel"], cbBrier = Err, times = times)
-results <- melt(results, id.vars = "times")
-
-ggplot(data = results, mapping = aes(x = times, y = value, col = variable)) +
-  geom_line() +
-  geom_rug(sides = "b") +
-  xlab("Times") +
-  ylab("Brier-Score")+
-  ggtitle("cbBrier using KM censoring probabilites")
-
-
-Err2<-brierScoreweibCens(predSurvs=absRiskcb, times, newDataX,newDataY)
-resultsCB<-rbind(rbind(data.frame(brierScore=Err,times=times,model="KMCensoring")),rbind(data.frame(brierScore=Err2,times=times,model="WeibullCensoring")),data.frame(brierScore = X2$Brier$score$Brier[X2$Brier$score$model == "PredictionModel"],times=times,model="riskRegressionCOX"))
-
-
-ggplot(data = resultsCB, mapping = aes(x = times, y = brierScore, col = model)) +
-  geom_line() +
-  geom_rug(sides = "b") +
-  xlab("Times") +
-  ylab("Brier-Score")+
-  ggtitle("KMcensoring vs. WeibullCensoring")
-
-
-
 brierScoreweibCens <- function(predSurvs= absRiskcb, times=times, newDataX=newDataX,newDataY=newDataY) {
   # inverse probability censoring weights
-  # note that we remove the last time, as this would give us 0. I shift all times by 1.
-  # IPCW=summary(survfit(Surv(time, event==0)~1, newData),c(min(times),head(times,-1)))$surv
+  
+  #DIFFERENCES START HERE
   yCens <- newDataY
   yCens[,1] <- yCens[,1]==0
   fitCens <- fitSmoothHazard(event~log(time) ,data= as.data.frame(yCens), time="time",ratio=100)
   IPCW.subject.times <- absoluteRisk(object = fitCens, time = times, s = "lambda.1se")
   IPCW.subject.times[, -c(1)] <- 1 - IPCW.subject.times[, -c(1)] # make it survival probabilities
   IPCW.subject.times <- rowMeans(IPCW.subject.times[-c(1), -c(1)]) # remove extra time at 0. Also remove the first column (times) as it matches perfectly now
+  #DIFFERENCES END HERE
   
   # Empty matrix that will be filled in with the following loop
   Score <- matrix(NA, nrow(predSurvs), ncol(predSurvs))
@@ -150,4 +86,59 @@ brierScoreweibCens <- function(predSurvs= absRiskcb, times=times, newDataX=newDa
   Err <- apply(Err, 1, mean)
   return(Err)
 }
+
+
+#Simulate/prepare data
+set.seed(18)
+
+astrain <- simActiveSurveillance(278)
+astrain$event <- astrain$event != 0 #take care of competing risks
+data.table::setorder(astrain, time, -event)
+astrainX <- model.matrix(event ~ . - time, data = astrain)[, -c(1)] #covariates
+astrainY <- data.matrix(astrain[, c(9, 8)]) #response
+
+newData <- simActiveSurveillance(208)
+newData$event <- newData$event != 0 #take care of competing risks
+data.table::setorder(newData, time, -event)
+newDataX <- model.matrix(event ~ . - time, data = newData)[, -c(1)] #covariates
+newDataY <- data.matrix(newData[, c(9, 8)]) #response
+
+times <- sort(unique(newData$time))
+
+
+# Fit a cox model
+coxfit <- coxph(Surv(time, event != 0) ~ ., data = astrain, x = TRUE)
+# Get Cox survivals
+coxPred <- summary(survfit(coxfit, newdata = newData), times = times)$surv
+# calculate the IPA/brier scores with riskRegression
+X2 <- Score(list("PredictionModel" = coxfit), data = newData, formula = Surv(time, event != 0) ~ 1, summary = "ipa", se.fit = 0L, metrics = "brier", contrasts = FALSE, times = times)
+
+
+#Fit Weibull casebase model
+cbModel <- fitSmoothHazard.fit(astrainX, astrainY, family = "glmnet", time = "time", event = "event", alpha = 0,formula_time = ~log(time), ratio = 100, standardize = TRUE)
+#we require a specific format for the probabilities
+absRiskcb <- absoluteRisk(object = cbModel, newdata = newDataX, time = times, s = "lambda.1se")
+absRiskcb[, -c(1)] <- 1 - absRiskcb[, -c(1)] # make it survival probabilities
+absRiskcb <- absRiskcb[-c(1), -c(1)] # remove extra time at 0. Also remove the first column (times) as it matches perfectly now
+
+
+#data is prepared to calculate brier scores
+kmCensoredBrierScores <- brierScoreKMCens(predSurvs = absRiskcb, times = times, newData = newData)
+weibullCensoredBrierScores<-brierScoreweibCens(predSurvs=absRiskcb, times, newDataX,newDataY)
+
+# restructuring to make plotting easier
+resultsCB<-rbind(rbind(data.frame(brierScore=kmCensoredBrierScores,times=times,model="KMCensoring")),rbind(data.frame(brierScore=weibullCensoredBrierScores,times=times,model="WeibullCensoring")),data.frame(brierScore = X2$Brier$score$Brier[X2$Brier$score$model == "PredictionModel"],times=times,model="riskRegressionCOX"))
+
+
+
+
+#brierScorePlot
+ggplot(data = resultsCB, mapping = aes(x = times, y = brierScore, col = model)) +
+  geom_line() +
+  geom_rug(sides = "b") +
+  xlab("Times") +
+  ylab("Brier-Score")+
+  ggtitle("KMcensoring vs. WeibullCensoring")
+
+
 
